4671e031a69f20da0cfd56693065fc10f3862d6b
consterboi <conrad.tillmann@outlook.com>
2024-12-03 17:33:08 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+let myCorg = corgio.create(SpriteKind.Player)
+myCorg.verticalMovement(true)
+myCorg.horizontalMovement(true)
+tiles.setCurrentTilemap(tilemap`level2`)
+myCorg.cameraFollow()
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
